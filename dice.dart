958dbdfae3f3a97f9bc79c863c67ba602b4dfa7d
Igor Drakulic <rogicilukard@gmail.com>
2026-06-29 16:19:07 +0200

    corrected counter

--- dice.dart
+++ dice.dart
@@ -36,7 +36,7 @@ void main() {
     print("WINER! Two sixes in a row! It took $throwCount throws");
     print("Your rolls are : $rolls");
   } else { 
-    print("Limit reached! $rolls rolls.");
+    print("Limit reached! $throwCount rolls.");
     print("All rolls: $rolls");
   } 
 }
