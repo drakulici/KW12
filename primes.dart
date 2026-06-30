60439708b57930224bd2539fb08415abb4b536af
Igor Drakulic <rogicilukard@gmail.com>
2026-06-30 10:06:30 +0200

    Prime with optimisation and comments

--- primes.dart
+++ primes.dart
@@ -1,4 +1,6 @@
 import "dart:io";
+// https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes EFFICIENCY 
+
 
 void main() { // MODULO % 
   
@@ -12,13 +14,16 @@ void main() { // MODULO %
     }
   }
 
+  // brute force - How many dividers does this number have? 
 
-  List<int> primes = []; // instance of empty primes list to store all the primes
+  Stopwatch bruteWatch = Stopwatch()..start();  // start timer to time efficiency 
 
-  for ( int i = 2; i <= max; i++) {  //for loop to parse from i to max(user input)
+  List<int> primes1 = []; // instance of empty primes list to store all the primes
+
+  for ( int i = 2; i <= max; i++ ) {  //for loop to parse from i to max(user input) 
     int foundDividers = 0;
 
-    for(int j = 1; j <= i - 1; j++) { // for every instance -1 (we dont need to check for 1) that gets to have max 2 foundDividers we add them to primes list 
+    for(int j = 1; j <= i; j++) { // for every instance -1 (we dont need to check for 1) that gets to have max 2 foundDividers we add them to primes list 
       if ( i % j == 0) {
         foundDividers++;
       }
@@ -26,8 +31,42 @@ void main() { // MODULO %
     
 
     if (foundDividers == 2) { 
-      primes.add(i);
+      primes1.add(i);
     }
   }
-  print("Your prime numbers are: $primes");
+  bruteWatch.stop(); // stop timer
+
+
+
+  print("*BRUTE* Your prime numbers are: $primes1");
+  print("Time it took: ${bruteWatch.elapsedMicroseconds} microseconds.");
+
+  // optimisation - does this nuber have more then 2 dividers? 
+
+
+  Stopwatch optiWatch = Stopwatch()..start(); // timer to time efficiency 
+
+  List<int> primes2 = []; // instance of empty list
+
+  for (int i = 2; i <= max; i++) { 
+    bool isPrime = true; // start with assumption the number is prime ( true ) and then disprove it by using modulo 
+      /* logic here is that prime has only 2 dividers 1 and itself  
+      example i = 9 -> dividers 1, 9 and algo finds imediately 3, so it breaks the loop... 
+        */
+    for ( int j = 2; j < i; j++) {
+      if (i % j == 0) { 
+        isPrime = false;
+        break;
+      }
+    }
+  if( isPrime) { //add i to the list
+    primes2.add(i);
+  }
+  }
+
+  optiWatch.stop(); // stop timer
+
+  print("###############################################################");
+  print("Optimised algo solution is : $primes2");
+  print("Time it took: ${optiWatch.elapsedMicroseconds} microseconds.");
 }
